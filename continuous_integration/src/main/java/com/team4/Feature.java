f236a9cb91877a48f02373338d40d25989d89fc3
Axcel Villagran <axvigarc@espol.edu.ec>
2024-06-26 14:18:03 -0500

    Membership information printer added

--- continuous_integration/src/main/java/com/team4/Feature.java
+++ continuous_integration/src/main/java/com/team4/Feature.java
@@ -1,5 +1,15 @@
 package com.team4;
 
 public class Feature {
-    
+    String description;
+    Double cost;
+    MembershipType plan;
+
+    public Feature(String description, Double cost, MembershipType plan){
+
+        this.description=description;
+        this.cost=cost;
+        this.plan=plan;
+    }
+
 }
--- continuous_integration/src/main/java/com/team4/Membership.java
+++ continuous_integration/src/main/java/com/team4/Membership.java
@@ -5,12 +5,12 @@ import java.util.List;
 
 public class Membership {
     
-    public Cost price;
+    public Cost totalCost;
     public MembershipType plan;
     public List<Feature> features= new ArrayList<>();
 
-    public Membership(Cost price, MembershipType plan, List<Feature>features){
-        this.price=price;
+    public Membership(Cost totalCost, MembershipType plan, List<Feature>features){
+        this.totalCost=totalCost;
         this.plan=plan;
         this.features=features;
 
@@ -18,7 +18,12 @@ public class Membership {
     
     public void printMembershipDetails(){
 
-        
+        System.out.println(plan.toString());
+        System.out.println("Selected Features");
+        for (Feature feature : features) {
+            System.out.println(feature);
+        }
+        System.out.println("Your total cost will be "+totalCost.price);
 
 
     }
